0e2894390caeebdc35d3cec9ffee43be35cc87bb
acacore <mensart81@outlook.com>
2025-08-26 14:10:11 +0000

    I have used SQL CREATE TABLE statements for each entity; ensured proper data types, primary keys, foreign keys, and constraints; and created necessary indexes on columns for optimal performance.

---
+++ database-script-0x01/schema.sql
@@ -0,0 +1,74 @@
+CREATE EXTENSION IF NOT EXIST "pgcrypto";
+
+CREATE TABLE users(
+    user_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    phone_number VARCHAR(20),
+    role VARCHAR(10) NOT NULL CHECK(role IN('guest', 'host', 'admin')),
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+)
+CREATE INDEX idx_users_user_id ON users(user_id);
+
+
+CREATE TABLE properties(
+    property_id PRIMARY KEY DEFAULT gen_random_uuid(),
+    host_id UUID NOT NULL REFERENCES users(user_id) ON DELETE CASCADE,
+    name VARCHAR(100) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    location VARCHAR(100) NOT NULL,
+    pricepernight DECIMAL(10,2) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE booking(
+    booking_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    property_id UUID NOT NULL REFERENCES properties(property_id) ON DELETE CASCADE,
+    user_id UUID NOT NULL REFERENCES users(user_id),
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    total_price DECIMA(10,2) NOT NULL,
+    status VARCHAR(10) NOT NULL CHECK(role IN('pending', 'confirmed', 'canceled')),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)
+
+CREATE INDEX idx_booking_property_id ON booking(property_id)
+CREATE INDEX idx_booking_booking_id ON users(user_id)
+
+
+CREATE TABLE payments(
+    payment_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    booking_id  UUID NOT NULL REFERENCES booking(booking_id) ON DELETE CASCADE,
+    amount DECIMAL(10,2) NOT NULL
+    payment_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    payment_method VARCHAR(15) NOT NULL CHECK(payment_method IN ('credit_card', 'paypal', 'stripe'))
+);
+
+
+CREATE TABLE review(
+    review_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    property_id UUID NOT NULL REFERENCES properties(property_id) ON DELETE CASCADE, 
+    user_id UUID NOT NULL REFERENCES users(user_id) ON DELETE CASCADE,
+    rating INTEGER NOT NULL CHECK(rating >= 1 AND rating <= 5),
+    comment TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE INDEX idx_review_property_id ON review(prperty_id);
+CREATE INDEX idx_review_user_id ON review(user_id);
+
+CREATE TABLE messages(
+    message_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    sender_id UUID NOT NULL REFERENCES users(user_id) ON DELETE CASCADE,  
+    recipient_id UUID NOT NULL REFERENCES users(user_id) ON DELETE CASCADE,  
+    message_body TEXT NOT NULL,
+    sent_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE INDEX idx_messages_sender_id ON messages(sender_id);
+CREATE INDEX idx_messages_recipient_id ON messages(recipient_id);
+
+
