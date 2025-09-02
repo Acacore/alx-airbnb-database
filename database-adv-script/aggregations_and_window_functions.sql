edc63e04943f1f91523df94d873ee39c0dba67b2
acacore <mensart81@outlook.com>
2025-09-02 14:08:00 +0000

    Necessary correction made

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -9,11 +9,11 @@ HAVING COUNT(booking)
 ORDER BY COUNT(booking) DESC)
 
 -- Use a window function (ROW_NUMBER, RANK) to rank properties based on the total number of bookings they have received.
--- aggregations_and_window_functions.sql
+-- Rank properties by total bookings using RANK()
 SELECT
   property_id,
   total_bookings,
-  ROW_NUMBER() OVER (ORDER BY total_bookings DESC) AS booking_rank
+  RANK() OVER (ORDER BY total_bookings DESC) AS booking_rank
 FROM (
   SELECT property_id,
          COUNT(*) AS total_bookings
