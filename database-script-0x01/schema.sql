001a31e9677c7275acb4aa60a6c9ddcd1a46d981
acacore <mensart81@outlook.com>
2025-09-02 10:42:26 +0000

    Data of each table provided

--- database-script-0x01/schema.sql
+++ database-script-0x01/schema.sql
@@ -1,6 +1,6 @@
-CREATE EXTENSION IF NOT EXIST "pgcrypto";
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 
-CREATE TABLE users(
+CREATE TABLE IF NOT EXISTS users(
     user_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     first_name VARCHAR(100) NOT NULL,
     last_name VARCHAR(100) NOT NULL,
@@ -9,22 +9,37 @@ CREATE TABLE users(
     phone_number VARCHAR(20),
     role VARCHAR(10) NOT NULL CHECK(role IN('guest', 'host', 'admin')),
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-)
+);
 CREATE INDEX idx_users_user_id ON users(user_id);
 
-
-CREATE TABLE properties(
+CREATE TABLE IF NOT EXISTS properties(
     property_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    host_id UUID NOT NULL REFERENCES users(user_id) ON DELETE CASCADE,
+    host_id UUID NOT NULL REFERENCES
+    users(user_id) ON DELETE CASCADE,
     name VARCHAR(100) NOT NULL,
     description VARCHAR(255) NOT NULL,
     location VARCHAR(100) NOT NULL,
     pricepernight DECIMAL(10,2) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE booking(
+-- Create a function that updates `updated_at` automatically
+CREATE OR REPLACE FUNCTION update_timestamp()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at = NOW();
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE TRIGGER set_timestamp
+BEFORE UPDATE ON users
+FOR EACH ROW
+EXECUTE FUNCTION update_timestamp();
+
+CREATE TABLE IF NOT EXISTS booking(
     booking_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     property_id UUID NOT NULL REFERENCES properties(property_id) ON DELETE CASCADE,
     user_id UUID NOT NULL REFERENCES users(user_id),
@@ -33,13 +48,13 @@ CREATE TABLE booking(
     total_price DECIMAL(10,2) NOT NULL,
     status VARCHAR(10) NOT NULL CHECK(status IN('pending', 'confirmed', 'canceled')),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-)
+);
 
-CREATE INDEX idx_booking_property_id ON booking(property_id)
-CREATE INDEX idx_booking_booking_id ON users(user_id)
+CREATE INDEX idx_booking_property_id ON booking(property_id);
+CREATE INDEX idx_booking_booking_id ON users(user_id);
 
 
-CREATE TABLE payments(
+CREATE TABLE IF NOT EXISTS payments(
     payment_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     booking_id  UUID NOT NULL REFERENCES booking(booking_id) ON DELETE CASCADE,
     amount DECIMAL(10,2) NOT NULL,
@@ -48,7 +63,7 @@ CREATE TABLE payments(
 );
 
 
-CREATE TABLE review(
+CREATE TABLE IF NOT EXISTS review(
     review_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     property_id UUID NOT NULL REFERENCES properties(property_id) ON DELETE CASCADE, 
     user_id UUID NOT NULL REFERENCES users(user_id) ON DELETE CASCADE,
@@ -60,7 +75,7 @@ CREATE TABLE review(
 CREATE INDEX idx_review_property_id ON review(property_id);
 CREATE INDEX idx_review_user_id ON review(user_id);
 
-CREATE TABLE messages(
+CREATE TABLE IF NOT EXISTS messages(
     message_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     sender_id UUID NOT NULL REFERENCES users(user_id) ON DELETE CASCADE,  
     recipient_id UUID NOT NULL REFERENCES users(user_id) ON DELETE CASCADE,  
@@ -70,6 +85,4 @@ CREATE TABLE messages(
 
 CREATE INDEX idx_messages_sender_id ON messages(sender_id);
 CREATE INDEX idx_messages_recipient_id ON messages(recipient_id);
-
-
 
