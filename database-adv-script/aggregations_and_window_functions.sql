0ce6c867cb53dbcd1a392a956f57e3fa615d9023
acacore <mensart81@outlook.com>
2025-09-02 14:05:37 +0000

    Correct aggregations_and_window_functions.sql doesn't contain: ['ROW_NUMBER()']

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -9,10 +9,15 @@ HAVING COUNT(booking)
 ORDER BY COUNT(booking) DESC)
 
 -- Use a window function (ROW_NUMBER, RANK) to rank properties based on the total number of bookings they have received.
-SELECT 
-    property_id,
-    COUNT(property_id) AS total_bookings,
-    RANK() OVER (ORDER BY COUNT(property_id) DESC) AS booking_rank
-FROM booking
-GROUP BY property_id
+-- aggregations_and_window_functions.sql
+SELECT
+  property_id,
+  total_bookings,
+  ROW_NUMBER() OVER (ORDER BY total_bookings DESC) AS booking_rank
+FROM (
+  SELECT property_id,
+         COUNT(*) AS total_bookings
+  FROM booking
+  GROUP BY property_id
+) AS aggregated
 ORDER BY total_bookings DESC;
